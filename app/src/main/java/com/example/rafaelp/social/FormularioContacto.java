0c134f7757cb6d8851089e2664453933e5c3c541
Rafael p <barboza.rafael.p@gmail.com>
2017-03-22 22:29:00 -0500

    Configurando libreria Mail

--- app/src/main/java/com/example/rafaelp/social/FormularioContacto.java
+++ app/src/main/java/com/example/rafaelp/social/FormularioContacto.java
@@ -54,6 +54,10 @@ public class FormularioContacto extends AppCompatActivity {
             public void onClick(View v) {
                 Properties props = new Properties();
                 props.put("mail.smtp.host", "smtp.gmail.com");
+                props.put("mail.smtp.socketFactory.port", "465");
+                props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+                props.put("mail.smtp.auth", "true");
+                props.put("mail.smtp.port", "465");
                 Session session = Session.getInstance(props, null);
 
                 try {
